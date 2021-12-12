e1a3a943fbfa938e5f7f1d5694946e56cba5a6cc
Joseph Kandi <joseph.kandi@absa.africa>
2021-12-12 09:22:49 +0200

    Connect to the server

--- src/components/Nickname.js
+++ src/components/Nickname.js
@@ -1,13 +1,29 @@
 import {  Input, Typography, Row, Col } from "antd";
+import {  useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 const { Title } = Typography;
 const ENTER_KEY = 13;
+const ADD_USER_KEY = "add user";
 
 function Nickname(){
 
+    const socket = io("https://chat.peruzal.com")
+ 
+    useEffect(() => {
+        socket.on('connect', () => {
+            // TODO - Set a visible icon for connection status
+            console.log('connected')
+        })
+
+        socket.on('connect_error', (error) => {
+            console.log(error)
+        })
+    }, [socket])
+
     const onHandleNickname =  (event) => {
-        if(event.keyCode  == ENTER_KEY){
-            
+        if(event.keyCode  === ENTER_KEY){
+            socket.emit(ADD_USER_KEY, event.target.value)
         }
     };
 
